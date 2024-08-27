27b346f986329fff46d824d5446eea25524a9f41
unknown <leee3240@gmail.com>
2024-08-28 00:43:05 +0900

    짝지어제거하기

---
+++ src/dh/programmers/짝지어제거하기.java
@@ -0,0 +1,36 @@
+package dh.programmers;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12973?language=java
+ */
+public class 짝지어제거하기 {
+    public static void main(String[] args) {
+        System.out.println("result = " + solution("baabaa"));
+        System.out.println("result = " + solution("cdcd"));
+    }
+
+    static int solution(String s)
+    {
+        int isLast = 0;
+        int size = s.length() - 1;
+
+        while(isLast == 0 && size > 0) {
+            for(int i = 0; i < size; i++) {
+                if(s.charAt(i) == s.charAt(i+1)) {
+                    String value1 = s.substring(0, i);
+                    String value2 = s.substring(i+2);
+
+                    s = value1 + value2;
+                    size = s.length()-1;
+                    break;
+                }
+
+                if(i+2 == s.length()) {
+                    isLast = 1;
+                }
+            }
+        }
+
+        return s.isEmpty() ? 1: 0;
+    }
+}
