50b3931d1788cd6a27b621096fd1155e64aa204c
skwzz <kidol9108@gmail.com>
2024-09-07 19:11:18 +0900

    [프로그래머스] 기지국 설치 문제풀이 - 풀이방식 변경 후 효율성 통과

--- src/kiwoong/programmers/lvl3/기지국설치.java
+++ src/kiwoong/programmers/lvl3/기지국설치.java
@@ -7,38 +7,38 @@ public class 기지국설치 {
     public static void main(String[] args) {
         System.out.println(solution(11, new int[]{4, 11}, 1)); // 3
         System.out.println(solution(16, new int[]{9}, 2)); // 3
+        System.out.println(solution(10, new int[]{1}, 1));
+        System.out.println(solution(10, new int[]{10}, 1));
+        System.out.println(solution(10, new int[]{1, 2, 3, 4, 5, 6 ,7 ,8 ,9 ,10}, 1));
+        System.out.println(solution(10, new int[]{1, 2, 3, 4, 5, 6 ,7 ,8 ,9 ,10}, 100));
+        System.out.println(solution(10, new int[]{5}, 5));
+        System.out.println(solution(10, new int[]{5}, 4));
     }
 
     static int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int[] arr = new int[n+1];
+        int quotient = w*2+1;
+        int pointer = 1;
         for(int i=0; i<stations.length; i++){
-            int l = stations[i] - w;
-            int r = stations[i] + w;
-            for(int j=l; j<=r; j++){
-                if(j <= 0 || j > n){
-                    continue;
-                }
-                arr[j] = 1;
+            int num = stations[i];
+            int left = num - w;
+            int right = num + w;
+
+            if(left>pointer){
+                int base = left-pointer;
+                answer += base/quotient;
+                answer += (base%quotient > 0) ? 1 : 0;
             }
-        }
-        //System.out.println(Arrays.toString(arr));
-        for (int i=1; i<arr.length; i++) {
-            if(arr[i] == 0){
-                int j = i;
-                while(j < arr.length && arr[j] == 0){
-                    j++;
-                }
-                int range = j - i;
-                int cnt = range / (2*w+1);
-                if(range % (2*w+1) != 0){
-                    cnt++;
-                }
-                answer += cnt;
-                i = j;
+            pointer = right + 1;
+            if(pointer > n){
+                break;
+            }
+            if(i==stations.length-1 && pointer <= n){
+                int base = n - right;
+                answer += base/quotient;
+                answer += (base%quotient > 0) ? 1 : 0;
             }
         }
         return answer;
     }
 }
-
